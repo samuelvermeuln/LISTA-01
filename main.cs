8e9f96c61a6a2c43600ed095a358af276adb1fef
samuel vermeuln <vermeuln@ucl.br>
2020-08-31 23:16:36 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,25 @@
+using System;
+/*
+1- Criar um programa que leia dois números inteiros, e que solicite ao usuário qual a operação 
+deseja realizar entre esses números. Caso o usuário digitar o caractere “*” será realizada uma multiplicação, 
+caso seja digitado o caractere “/” será realizada uma divisão, caso seja digitado o caractere “+” 
+será realizado uma adição, e caso seja digitado o caractere “–” será realizada uma subtração.
+*/
+
+class MainClass {
+  public static void calculadora () {
+    Console.WriteLine ("Calculadora !");
+
+    double num1, num2, result;
+    string a, b;
+    char c;
+
+    Console.WriteLine ("digite o primeiro valor: ");
+    
+    Console.WriteLine ();
+  }
+
+  public static void Main (string[] args) {
+    calculadora();
+  }
+}
